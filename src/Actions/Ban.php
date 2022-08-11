7115c2fb7a1436b35214dc9e386a33bf6e0fe5ad
Roy Shay <roy.shay@gmail.com>
2022-08-11 17:47:24 +0100

    linting

--- src/Actions/Ban.php
+++ src/Actions/Ban.php
@@ -32,8 +32,8 @@ class Ban extends Action
     /**
      * Perform the action on the given models.
      *
-     * @param  \Laravel\Nova\Fields\ActionFields $fields
-     * @param  \Illuminate\Support\Collection $models
+     * @param ActionFields $fields
+     * @param Collection $models
      * @return mixed
      */
     public function handle(ActionFields $fields, Collection $models)
--- src/Actions/Unban.php
+++ src/Actions/Unban.php
@@ -30,8 +30,8 @@ class Unban extends Action
     /**
      * Perform the action on the given models.
      *
-     * @param  \Laravel\Nova\Fields\ActionFields  $fields
-     * @param  \Illuminate\Support\Collection  $models
+     * @param ActionFields $fields
+     * @param Collection $models
      * @return mixed
      */
     public function handle(ActionFields $fields, Collection $models)
